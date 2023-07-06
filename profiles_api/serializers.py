3329d3cc8cc9cabee8b2f5b2d6e0b5ea4dc9831d
iykedaniel <iykedaniel3737@gmail.com>
2023-07-06 14:18:49 +0100

    added searching to user proffiles

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,40 @@
 from rest_framework import serializers
 
+
+
+
+from profiles_api import models
+
+
+class UserProfileSerializer(serializers.Serializer):
+    """Serializes a name ield for testing our ApIViews"""
+    name = serializers.CharField(max_length=10)
+
+
 class HelloSerializer(serializers.Serializer):
     """Serializes  a name field for testing our APIviews"""
     name = serializers.CharField(max_length=10)
+
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'imput_type': 'password'}
+
+            }
+        }
+
+
+    def create(self, validate_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validate_data['email'],
+            name = validate_data['name'],
+            password = validate_data['password']
+
+        )
+
+        return user
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
 
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 
 
@@ -100,6 +105,18 @@ class HelloViewSet(viewsets.ViewSet):
     def delete(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE',})
+    
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    '''Handle creating and updating profiles'''
+    serializer_class =serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
 
     
     
